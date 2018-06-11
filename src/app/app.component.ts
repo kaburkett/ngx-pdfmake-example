1d8b8a2b03f77424103d53d3926912e9b0bf34ec
Kyle A. Burkett <kyle.burkett@wwinc.com>
2018-06-11 13:08:41 -0500

    update from Angular 4 to 6+

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,25 @@ export class AppComponent {
 constructor(){
      //called first time before the ngOnInit()
      pdfMake.vfs = pdfFonts.pdfMake.vfs;
-      var dd = { content: 'This is an sample PDF printed with pdfMake' };
+      var dd = {
+        content: [
+          {
+            layout: 'lightHorizontalLines', // optional
+            table: {
+              // headers are automatically repeated if the table spans over multiple pages
+              // you can declare how many rows should be treated as headers
+              headerRows: 1,
+              widths: [ '*', 'auto', 100, '*' ],
+      
+              body: [
+                [ 'First', 'Second', 'Third', 'The last one' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ { text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4' ]
+              ]
+            }
+          }
+        ]
+      };
     pdfMake.createPdf(dd).download();
   }
     
